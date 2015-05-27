225f71fa577a8916e39338ac46463515c2ddf01f
Gandhar <gandhark@cybage.com>
2015-05-27 12:12:49 +0530

    df

--- ecommDeployVhost.sh
+++ ecommDeployVhost.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
- echo  "127.0.0.1 ecommercecd">>sudo /etc/hosts
+ echo  "127.0.0.1 ecomm_project">> /etc/hosts
 echo "Creating VHOST file "
  echo '<VirtualHost *:80>
-    <Directory "/opt/rh/httpd24/root/var/www/html/ecommercecd">
+    <Directory "/opt/rh/httpd24/root/var/www/html/ecomm_project">
         Options Indexes FollowSymLinks Includes ExecCGI
         Order allow,deny
         Allow from all
@@ -16,15 +16,15 @@ echo "Creating VHOST file "
         php_value allow_call_time_pass_reference 0
     </IfModule>
     DirectoryIndex index.php
-    DocumentRoot "/opt/rh/httpd24/root/var/www/html/ecommercecd"
-    ServerName ecommercecd
-</VirtualHost>' >>/opt/rh/httpd24/root/etc/httpd/conf.d/ecommercecd.conf
+    DocumentRoot "/opt/rh/httpd24/root/var/www/html/ecomm_project"
+    ServerName ecomm_project
+</VirtualHost>' >>/opt/rh/httpd24/root/etc/httpd/conf.d/ecomm_project.conf
 
 
  /etc/init.d/httpd24-httpd restart
 
- firefox -new-window ecommercecd
+ firefox -new-window ecomm_project
 echo "Hurrah! Got success"
 cd /opt/rh/httpd24/root/etc/httpd/conf.d/
-rm -rf ecommerce-cd.conf
+rm -rf ecomm_project.conf
 
